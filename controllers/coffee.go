8b0a24653cf118feec2f5659fda8b04030c2f799
siamliam12 <129056535+siamliam12@users.noreply.github.com>
2023-10-26 15:25:31 +0600

    A lot of error fixes
    Re-writing the create coffee feature so that I can upload an Image of a coffee. It took a while to build and debug but finally gridfs is working for storing the data in the database

--- controllers/coffee.go
+++ controllers/coffee.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"context"
+	"io"
 	"net/http"
+	"regexp"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -12,24 +14,96 @@ import (
 	"github.com/siamliam12/coffee_store/utils"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/gridfs"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var coffeeCollection *mongo.Collection = utils.GetCollection(utils.DB, "coffee")
-var validate = validator.New()
+var coffeeCollection *mongo.Collection = utils.GetCollection(utils.DB, "coffee") //coming for models to collect the collection from db
+var validate = validator.New()                                                   //validator libary
 
+// function to create a coffee in a database
 func CreateCoffee(c *fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	var coffee models.Coffee
 	defer cancel()
 
+	//check if file is present in request body or not
+	fileHeader, err := c.FormFile("image")
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	//check if file is of type image or not
+	fileExtension := regexp.MustCompile(`\.[a-zA-Z0-9]+$`).FindString(fileHeader.Filename)
+	if fileExtension != ".jpg" && fileExtension != ".jpeg" && fileExtension != ".png" {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   "Invalid file type",
+		})
+	}
+
+	//read file content
+	file, err := fileHeader.Open()
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
 	//validate the request body
 	if err := c.BodyParser(&coffee); err != nil {
+		//get the error message the response template
 		return c.Status(http.StatusBadRequest).JSON(responses.CoffeeResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
 	}
 	//USE the validator library to validate required fields
 	if validationErr := validate.Struct(&coffee); validationErr != nil {
 		return c.Status(http.StatusBadRequest).JSON(responses.CoffeeResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
 	}
+	// Create db connection
+	db := utils.ConnectDB().Database("cafe")
+
+	//create bucket
+	opts := options.GridFSBucket().SetName("images")
+	bucket, err := gridfs.NewBucket(db, opts)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": true,
+			"msg": err.Error(),
+		})
+	}
+	// Upload file to GridFS bucket
+	uploadStream, err := bucket.OpenUploadStream(fileHeader.Filename, options.GridFSUpload().SetMetadata(fiber.Map{"ext": fileExtension}))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	//close upload stream
+	fieldId := uploadStream.FileID
+	defer uploadStream.Close()
+
+	//write file content to upload stream
+	filesize, err := uploadStream.Write(content)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
 	newCoffee := models.Coffee{
 		Id:          primitive.NewObjectID(),
 		Name:        coffee.Name,
@@ -43,6 +117,11 @@ func CreateCoffee(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(http.StatusBadRequest).JSON(responses.CoffeeResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
 	}
-	return c.Status(http.StatusCreated).JSON(responses.CoffeeResponse{Status: http.StatusCreated, Message: "success", Data: &fiber.Map{"data": result}})
-
+	return c.Status(http.StatusCreated).JSON(responses.CoffeeResponse{Status: http.StatusCreated, Message: "success", Data: &fiber.Map{"data": result,
+		"msg": "Image uploaded successfully",
+		"image": fiber.Map{
+			"id":   fieldId,
+			"name": fileHeader.Filename,
+			"size": filesize,
+		}}})
 }
--- go.mod
+++ go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.3.1 // indirect
-	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
